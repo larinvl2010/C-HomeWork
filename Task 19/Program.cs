fe1fb827ba26462ee2e39ff9f431034fc12be597
Vladimir <larinvl2010@mail.ru>
2023-01-29 17:44:06 +0300

    Третье ДЗ

---
+++ Task 19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+Console.WriteLine("Enter a number");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num / 10000;
+int num2 = num / 1000;
+num2 = num2 % 10;
+int num4 = num / 10;
+num4 = num4 % 10;
+int num5 = num % 10;
+if (num>9999 && num < 100000) 
+{    if (num1 == num5 && num2 ==num4)
+Console.WriteLine("Number is polindrom");
+    else
+Console.WriteLine("Number is not polindrom");
+}
+else
+{
+    Console.WriteLine("Uncorrect number");
+}
---
+++ Task 21/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("Enter the coordinates of the first point");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the coordinates of the second point");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Round(Math.Sqrt(Math.Pow((x2 - x1), 2)+ Math.Pow((y2 - y1),2)+Math.Pow((z2 - z1),2)), 3);
+Console.WriteLine(result);
