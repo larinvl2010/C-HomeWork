c28b67d1028f1938a9cc1a59089111a95ba7c49f
Vladimir <larinvl2010@mail.ru>
2023-01-31 05:43:11 +0300

    Четвёртое ДЗ

---
+++ Task 25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+int MultiPly(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.WriteLine($"Произведение числа {A} в степени {B} равна {MultiPly(A,B)} ");
+
---
+++ Task 27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+Console.Write("Введите число: ");
+string numstr=(Console.ReadLine());
+int Sum(string numstr)
+{
+int num = Convert.ToInt32(numstr);
+int index = 0;
+int sum = 0;
+for (int i = 1; i <= numstr.Length; i++)
+{
+    index=num%10;
+    num=num/10;
+    sum = sum+index;
+}
+return sum;
+}
+Console.WriteLine($"Сумма чисел {numstr} равна {Sum(numstr)} ");
