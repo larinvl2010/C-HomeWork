fefa0e604dca4a096ebb0166347a9e6e41a3c6f1
Vladimir <larinvl2010@mail.ru>
2023-02-03 06:33:33 +0300

    Пятое ДЗ

---
+++ Task 34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int[] FillArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i]=new Random().Next(100,1000);
+    }
+    return array;
+}
+int EvenNumbers(int[] array)
+{
+    int count=0;
+    foreach (int item in array)
+    {
+        if(item%2==0)
+        count++;
+    }
+    return count;
+}
+Console.WriteLine("Enter the length of the array");
+int length=Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(length);
+Console.WriteLine("the number of even elements in the array [" + string.Join(" ",array)+"] : "+EvenNumbers(array));
---
+++ Task 36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int[] FillArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i]=new Random().Next(0,10);
+    }
+    return array;
+}
+int SumEvenNumbers(int[] array)
+{
+    int i=0;
+    int sum=0;
+    foreach (int item in array)
+    {
+        if(i%2==1)
+        sum+=item;
+        i++;
+    }
+    return sum;
+}
+Console.WriteLine("Enter the length of the array");
+int length=Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(length);
+Console.WriteLine("The sum of the elements in the array in odd positions [" + string.Join(", ",array)+"] : "+SumEvenNumbers(array));
