e61e9e1870e172475cb1b59d4170ad98dd0cbc11
Vladimir <larinvl2010@mail.ru>
2023-02-16 21:56:46 +0300

    Последнее ДЗ

---
+++ Task 66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Enter a number M:");
+int m =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a number N:");
+int n =Convert.ToInt32(Console.ReadLine());
+
+
+int SumNumbers(int i, int j)
+{
+   if (i>j)
+    {
+    int temp=j;
+    j=i;
+    i=temp;
+    } 
+    if (i==j) return i;
+    else return i+SumNumbers(i+1,j);
+}
+Console.WriteLine("Summa beetween M and N = "+SumNumbers(m,n));
---
+++ Task 68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Enter a number M:");
+int m =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a number N:");
+int n =Convert.ToInt32(Console.ReadLine());
+
+
+int AkkermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AkkermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AkkermannFunction(numberM - 1, AkkermannFunction(numberM, numberN - 1));
+return AkkermannFunction(numberM, numberN);
+}
+
+Console.WriteLine("Akkerman function for numbers A("+m+" "+n+")= "+AckermannFunction(m, n));
